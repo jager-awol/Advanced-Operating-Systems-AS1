7af06fc57736ab8c0df09f6a03478f528fe261bb
Jager <jaake.4232@gmail.com>
2026-03-03 01:27:00 +0000

    Addition of printing cpu and memory status to option 1

--- task1script.sh
+++ task1script.sh
@@ -10,7 +10,13 @@ select opt in "${options[@]}"
 do
 	case $opt in 
 		"Resource Monitoring and Management")
-			echo "Option 1 chosen";;
+			echo "Resource Monitoring and Management chosen...."
+			echo ""
+			echo "Displaying Memory Status:"
+			free -t
+			echo ""
+			echo "Displaying CPU Status:"
+			top -bn1 | grep "Cpu(s)";;
 		"Disk Inspection and Log Archiving")
 			echo "Option 2 chosen";;
 		"Logging System")
