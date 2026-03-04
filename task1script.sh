3a13e0e59500d22881290a79e02a5d543e4d09cc
Jager <jaake.4232@gmail.com>
2026-03-04 18:40:29 +0000

    Update task1script.sh with terminate functionality, confirmation of termination and cancellation of termination if critical process. Option 1 done.

--- task1script.sh
+++ task1script.sh
@@ -11,22 +11,44 @@ do
 	case $opt in 
 		"Resource Monitoring and Management")
 			echo "Resource Monitoring and Management chosen...."
-			echo ""
-			echo "Displaying Memory Status:"
+			echo -e "\nDisplaying Memory Status:"
 			free -t
-			echo ""
-			echo "Displaying CPU Status:"
+			echo -e "\nDisplaying CPU Status:"
 			top -bn1 | grep "Cpu(s)"
-			top -o %MEM -d 5 -b -n 1|grep "load average" -A 16
-			select opt2 in "Terminate Process" Back
+			echo ""
+			select opt2 in "View Processes" "Terminate Process" Back
 			do
 				case $opt2 in 
+					"View Processes")
+						top -o %MEM -d 5 -b -n 1|grep "load average" -A 16
+						echo "";;
 					"Terminate Process")
-						echo "Terminate";;
+						# tPID="USER INPUT"
+						read -p "Enter PID to terminate: " tPID
+						# confirm="USER INPUT"
+						printf "Are you sure you want to terminate %s?\nY/N: " "$tPID"
+						read confirm
+						case $confirm in
+							"Y")
+								echo -e "\nTerminating....\n"
+								cmd=$(ps -o comm= -p "$tPID")
+								case $cmd in 
+									systemd|init|bash|sshd)
+										echo -e "Error - Critical System Process. Cancelling....\n";;
+									*)
+									kill $tPID;;
+								esac;;
+							"N")
+								echo -e "\nCancelling....\n";;
+							*)
+							echo -e "\nInvalid Choice\n"
+						esac
+						REPLY=
+						;;
 					"Back")
 						break;;
 					*)
-					echo "Invalid Choice, please select again.";;
+					echo -e "Invalid Choice, please select again.\n";;
 				esac
 			done
 			REPLY=
