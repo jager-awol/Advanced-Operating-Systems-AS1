e3cff9fcf23ec8951b9f21d277ac8ab266d90e85
Jager <jaake.4232@gmail.com>
2026-03-20 05:35:51 +0000

    Options 2 & 3 completed
    ArchiveLogs Implemented, compresses .log files over 50mb into ArchiveLogs directory, warns user if ArchiveLogs is over 1gb.
    Logs all activities in system_monitor_log.txt with timestamps.

--- task1script.sh
+++ task1script.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/bash
 
+echo "System monitor started at $(date '+%H:%M:%S %d-%m-%Y')"  >> system_monitor_log.txt
+
 echo "This menu is used for selecting  operations."
 
 PS3="Select an Option (Ex. 1, 2, 3): "
 
-options=("Resource Monitoring and Management" "Disk Inspection and Log Archiving" "Logging System" "Exit")
+options=("Resource Monitoring and Management" "Disk Inspection and Log Archiving" "View Log" "Exit") 
 
 select opt in "${options[@]}"
 do
@@ -21,7 +23,9 @@ do
 				case $opt2 in 
 					"View Processes")
 						top -o %MEM -d 5 -b -n 1|grep "load average" -A 16
-						echo "";;
+						echo ""
+						echo "Processes viewed at $(date '+%H:%M:%S %d-%m-%Y')" >> system_monitor_log.txt
+						;;
 					"Terminate Process")
 						# tPID="User input for PID to terminate"
 						read -p "Enter PID to terminate: " tPID
@@ -36,7 +40,9 @@ do
 									systemd|init|bash|sshd)
 										echo -e "Error - Critical System Process. Cancelling....\n";;
 									*)
-									kill $tPID;;
+									kill $tPID
+									echo "PID $tPID terminated at $(date '+%H:%M:%S %d-%m-%Y')" >> system_monitor_log.txt
+									;;
 								esac;;
 							"N")
 								echo -e "\nCancelling....\n";;
@@ -65,9 +71,31 @@ do
 				# dDirectory="User input for directory to inspect"
 				printf "Enter a directory to inspect:%s\n"
 				read dDirectory
-				du -h -s $dDirectory
+				du -h -s "$dDirectory"
+				echo "Directory $dDirectory inspected at $(date '+%H:%M:%S %d-%m-%Y')" >> system_monitor_log.txt
+				# confirm="User input for archive log and exit decisions"
+				printf "Do you want to archive large log files?%s?\nY/N: "
+				read confirm
+				case $confirm in
+					"Y")
+						mkdir -p ArchiveLogs
+						find "$dDirectory" -type f -name "*.log" -size +50M -print0 | while IFS= read -r -d '' file
+						do
+							#filename="file name to save compressed file as."
+							filename=$(basename "$file")
+							gzip -c  "$file" > "ArchiveLogs/$filename.gz" && rm "$file"
+							echo "$filename compressed to ArchiveLogs at $(date '+%H:%M:%S %d-%m-%Y')" >> system_monitor_log.txt
+						done
+						if [ "$(du -sm ArchiveLogs | cut -f1)" -gt 1024 ]; then
+							echo "Warning - ArchiveLogs directory is over  1GB"
+						fi
+						;;
+					"N")
+						;;
+					*)
+					echo -e "\n Invalid Choice \n"
+				esac
 				echo ""
-				# confirm="User input for exiting to first menu or continuing"
 				printf "Do you want to inspect another directory?%s?\nY/N: "
 				read confirm
 				case $confirm in 
@@ -83,16 +111,16 @@ do
 			done
 			REPLY=
 			;;
-		"Logging System")
-			echo "Option 3 chosen";;
+		"View Log")
+			cat system_monitor_log.txt
+			echo ""
+			REPLY=
+			;;
 		"Exit")
 			echo "Exiting...."
+			echo -e "System Monitor terminated at $(date '+%H:%M:%S %d-%m-%Y')\n" >> system_monitor_log.txt
 			break;;
 		*)
-		echo "Invalid Choice, please select again.";;
-	esac
-done
-
 		echo "Invalid Choice, please select again.";;
 	esac
 done
