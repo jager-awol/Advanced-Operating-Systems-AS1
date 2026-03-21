d48cdb8dbc257a21180dca55edae4f637c068f91
Jager <jaake.4232@gmail.com>
2026-03-21 05:38:42 +0000

    updated task2script.sh with ability to process jobs, prioritising jobs on higher priority attribute value and waiting the estimated completion time to simulate processing.

--- task2script.sh
+++ task2script.sh
@@ -8,15 +8,17 @@ select opt in "${options[@]}"
 do
 	case $opt in
 		"View Pending")
-			echo "Task ID, Student ID, Task Name, Estimated Time, Priority"
+			echo -e "\nTask ID, Student ID, Task Name, Estimated Time, Priority"
 			while IFS=',' read -r TID SID name estTime priority
 			do
 				echo "$TID, $SID, $name, $estTime, $priority"
 			done < "taskPending.txt"
+			echo ""
 			REPLY=
 			;;
 		"Submit Job")
 			#TID,SID,name,estTime,priority= "temporary variables to hold task attributes before it is saved to task list"
+			echo ""
 			read -p "Enter Student ID: " SID
 			read -p "Enter Task Name: " name
 			read -p "Enter Task Priority: " priority
@@ -24,21 +26,47 @@ do
 			estTime=$(( RANDOM % 19 + 2 ))
 			# assigning  TID by incrementing on TID of previous task list entry.
 			IFS=',' read -r TID _ <<< "$(tail -n 1 taskPending.txt)"
-			((TID++))
+			if  [[ -z "$TID" ]]; then
+				TID=1
+			else
+				((TID++))
+			fi
 			echo $TID,$SID,$name,$estTime,$priority >> taskPending.txt
+			echo -e "\nJob added to pending list.\n"
+			REPLY=
 			;;
 		"Process Jobs")
-			#highestPriority= "current highest priority read"
-			highestPriority=-1
-			#selected= "current task selected by priority comparison"
-			selected=""
-			while IFS=',' read -r TID SID name estTime priority
+			while :
 			do
-				if (( priority > highestPriority )); then
-					highestPriority = $priority
-					selected="$TID,$SID,$name,$estTime,$priority"
-				fi
-			done < "taskPending.txt"
+				#highestPriority= "current highest priority read"
+				highestPriority=-1
+				#selected= "current task selected by priority comparison"
+				selected=""
+				while IFS=',' read -r TID SID name estTime priority
+				do
+					if (( priority > highestPriority )); then
+						highestPriority=$priority
+						selected="$TID,$SID,$name,$estTime,$priority"
+					fi
+				done < "taskPending.txt"
+				IFS=',' read -r TID SID name estTime priority <<< "$selected"
+				echo "running task $TID $name by $SID at priority $priority. Estimated $estTime until completion...."
+				sleep $estTime
+				echo "Task $TID complete. Saved to taskComplete.txt."
+				printf "Continue next job? \n (Y/N): "
+				read confirm
+				case $confirm in
+					"Y")
+						;;
+					"N")
+						echo -e "\nReturning to menu....\n"
+						break;;
+					*)
+					echo -e "\nInvalid Choice\n"
+					break
+				esac
+			done
+			REPLY=
 			;;
 		"View Completed")
 			echo "option 4 selected"
