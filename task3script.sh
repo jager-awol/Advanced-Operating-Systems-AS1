eb64cc9650cd44f8d0761633aaae5de1e36fe544
Jager <jaake.4232@gmail.com>
2026-03-22 06:04:15 +0000

    Updated task3script.sh with menu once user is logged in, providing option to provide submission or quit, currently only with quitting functionality.

--- task3script.sh
+++ task3script.sh
@@ -20,16 +20,50 @@ do
 	done < userDetails.txt
 	if [[ "$login" == "false" ]]; then
 		((attempt++))
+		echo -e "\nIncorrect username or password. Please try again.\n"
 	else
 		break
 	fi
 done
 
 if $login; then
-	echo "login success, continuing"
+	#script executes if user successfully logs on
+	echo "Login successful."
+	while :
+	do
+		PS3="Select an Option (Ex. 1, 2): "
+		options=("Upload Submission" "Quit")
+		select opt in "${options[@]}"
+		do
+			case $opt in
+				"Upload Submission")
+					echo "Submission uploaded"
+					;;
+				"Quit")
+					printf "Are you sure you want to quit? \n (Y/N): "
+					read confirm
+					case $confirm in
+						"Y")
+							echo "Exiting...."
+							sleep 5
+							exit
+							;;
+						"N")
+							;;
+						*)
+						echo -e "Invalid Choice, please select again.\n"
+						;;
+					esac
+					REPLY=
+					;;
+				*)
+				echo "Invalid Choice, please select again.";;
+			esac
+		done
+	done
 else
+	#script executes is user is unsuccessful at logging in within 3 attempts
 	echo "login failed, exiting"
 	sleep 5
 	exit
 fi
-
