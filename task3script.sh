3189751ed5b793b608065529bfe73445436bb8b8
Jager <jaake.4232@gmail.com>
2026-03-22 07:09:02 +0000

    updated task3script.sh with functionality on upload submission option. script verifies correct size, file type and no duplicate files. records failed login attempts and successful submissions in submission_Log.txt

--- task3script.sh
+++ task3script.sh
@@ -3,8 +3,11 @@
 echo "Please log in to upload submission."
 #attempt = keep track of login attempts to stop at over 3 tries
 #login = keep track of whether login is successful so script continues to submission if login succeeds
+#userInput = keep track of username for logging purposes
 attempt=0
 login=false
+userInput=""
+mkdir -p "submissions"
 
 while (( "$attempt" < 3 ))
 do
@@ -21,6 +24,7 @@ do
 	if [[ "$login" == "false" ]]; then
 		((attempt++))
 		echo -e "\nIncorrect username or password. Please try again.\n"
+		echo "Failed login attempt at $(date '+%H:%M:%S %d-%m-%Y')" >> submission_Log.txt
 	else
 		break
 	fi
@@ -37,7 +41,39 @@ if $login; then
 		do
 			case $opt in
 				"Upload Submission")
-					echo "Submission uploaded"
+					printf "Please enter filepath for submission file. Only accepts pdf or docx files under 5MB (Ex. work/submission.pdf): "
+					read submission
+					if [[ ! -f "$submission" ]]; then
+						echo "Error: File does not exist"
+						continue
+					fi
+					#fileExt = extracted file extension for submission file
+					fileExt="${submission##*.}"
+					if  [[ "$fileExt" != "docx" && "$fileExt" != "pdf" ]]; then
+						echo "Error: incorrect filetype. Only .pdf or .docx file are accepted."
+						continue
+					fi
+					#fileSize = file size of provided submission file to compare against 5mb
+					fileSize =$(stat -c%s "$submission")
+					if (( fileSize > 5242880 )); then
+						echo "Error: File over 5MB maximum size."
+						continue
+					fi
+					#fileName = file name of submission to check against files already in submission directory
+					fileName=$(basename "$submission")
+					if [[ -e "submissions/$fileName" ]]; then
+						echo "Error: Duplicate file name already in submissions directory."
+						continue
+					fi
+					#fileHash = hash of file contents to compare against contents of other files already in submission directory
+					fileHash=$(sha256sum "$submission" | awk '{print $1}')
+					if sha256sum "submissions"/* 2>dev/null | awk '{print $1}' | grep -q "^$fileHash$"; then
+						echo "Error: Duplicate file contents already in submissions directory."
+						continue
+					fi
+					mv $submission submissions
+					echo "$fileName submitted by $userInput at $(date '+%H:%M:%S %d-%m-%Y')" >> submission_Log.txt
+					echo "Submission successfully recieved."
 					;;
 				"Quit")
 					printf "Are you sure you want to quit? \n (Y/N): "
